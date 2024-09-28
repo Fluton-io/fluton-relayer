7205abd5e85c02c12f7eccd004cec8b25574781c
burakinkaya <burakinkaya@sabanciuniv.edu>
2024-09-28 13:28:06 +0300

    feat: Websocket Client initialized

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
+import { io } from "socket.io-client";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () => {
+  console.log(`Relayer connected with ID: ${socket.id}`);
+});
+
+socket.on("disconnect", () => {
+  console.log("Relayer disconnected");
+});
+
+socket.on("giveOffers", (intent) => {
+  console.log("Received intent:", intent);
+
+  const parsedIntent = JSON.parse(intent);
+
+  const fee = Math.floor(Math.random() * 10) + 1;
+  const price = 1.01;
+  const targetAmount = price * Number(parsedIntent.amount) - fee;
+
+  console.log(`Parsed intent:`, parsedIntent);
+  console.log(`Calculated targetAmount: ${targetAmount}, fee: ${fee}`);
+});
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[relayer]: Relayer is running at http://localhost:${port}`);
 });
