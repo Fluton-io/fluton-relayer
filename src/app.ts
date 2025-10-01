e082cf7ccdca003b6e6010ced1a9f65191dddd1f
cozfuttu <canozfuttu07@gmail.com>
2025-10-01 16:38:57 +0300

    chore: added rpc socket close functionality

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import { PORT } from "./config/env";
 import { createSocket } from "./services/socketService";
 import { listenBridgeEvents } from "./services/listenerService";
+import { websocketClients } from "./config/client";
 
 const app: Express = express();
 const port = PORT;
@@ -16,7 +17,15 @@ export const startServer = () => {
   });
 
   process.on("SIGINT", async () => {
-    console.log("About to close");
+    console.log("About to close, please wait.");
+
+    // Close all websocket connections
+    await Promise.all(
+      websocketClients.map(async ({ client }) => {
+        const rpcClient = await client.transport.getRpcClient();
+        return rpcClient.close();
+      })
+    );
 
     server.close();
     socket.close();
